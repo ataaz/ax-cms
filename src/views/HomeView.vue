6d9f81017b5431ab9a1f83a5e916981e266d7460
atashaikh-MRM <ata.shaikh@mrm.com>
2026-03-20 02:43:44 +0500

    Remove vue.zip from remote, restore .gitattributes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import BlogList from '@/components/BlogList.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <BlogList />
   </main>
 </template>
